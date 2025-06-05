204ec6164fa8c80c0b38035e8ba581e33169021e
Tommy Jingles <tommy.jingles@yahoo.com>
2025-06-04 20:59:27 -0500

    Revert "Revert "first pass of terrain_plugin using mesh vertex colors""
    This reverts commit 028c9473d81b175f1075d6286d6860f0c499965d.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,2 @@
-
-use bevy::prelude::*;
-
-pub struct AppPlugin;
-
-impl Plugin for AppPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_plugins(
-            DefaultPlugins
-                .set(WindowPlugin {
-                    primary_window: Window {
-                        title: "Thrive".to_string(),
-                        ..default()
-                    }
-                    .into(),
-                    ..default()
-                })
-        );
-    }
-}
+pub mod camera;
+pub mod terrain;
